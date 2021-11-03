f1e07ad912aafcda7c0ba8fac3c72720a4bdde14
roberto <rtorres@cifpbmoll.eu>
2021-11-03 16:06:58 +0100

    Initial commit

---
+++ app/src/main/java/com/example/scrollingtext/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.scrollingtext;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    int num;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void Add_Comment(View view) {
+        Button but = findViewById(R.id.new_commentary);
+        EditText editText = (EditText) findViewById(R.id.articleComment);
+        TextView txt = (TextView) findViewById(R.id.article);
+        but.setText("Send");
+        editText.setVisibility(view.VISIBLE);
+        but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (but.getText() == "Send") {
+                    String oldText = txt.getText().toString();
+                    String comment = editText.getText().toString();
+                    num++;
+                    txt.setText(oldText + "\n" + "\n" + "Comment_" + num + " : " + comment + "\n");
+                    but.setText("Add Comment");
+                    editText.setVisibility(view.INVISIBLE);
+                } else {
+                    editText.setVisibility(View.VISIBLE);
+                    but.setText("Send");
+                }
+            }
+        });
+    }
+}
